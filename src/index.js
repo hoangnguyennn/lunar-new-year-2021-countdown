ac23a326aa0a18ea39a468ec11b7b0c6b2c9cc2e
hoangnguyennn <nvanhoang188@gmail.com>
2020-12-06 11:57:42 +0700

    remove moment.js and use vanilla js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './style.scss'
 
-const TET = new Date('February 12, 2021 00:00:00 GMT+07:00').toUTCString()
-const TET_MOMENT = moment(TET)
+const TET = new Date('February 12, 2021 00:00:00 GMT+07:00')
 
 const ONE_SEC = 1000
 const ONE_MIN = 60 * ONE_SEC
@@ -10,8 +9,8 @@ const ONE_DAY = 24 * ONE_HOUR
 
 document.addEventListener('DOMContentLoaded', () => {
   function calculateAndSetTime() {
-    let now = moment(new Date().toUTCString())
-    let secondsDiff = TET_MOMENT - now
+    let now = new Date()
+    let secondsDiff = TET.getTime() - now.getTime()
 
     let days = Math.floor(secondsDiff / ONE_DAY)
     secondsDiff -= days * ONE_DAY
@@ -22,7 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let minutes = Math.floor(secondsDiff / ONE_MIN)
     secondsDiff -= minutes * ONE_MIN
 
-    let seconds = secondsDiff / ONE_SEC
+    let seconds = Math.ceil(secondsDiff / ONE_SEC)
 
     document.querySelector('.days > .box').innerHTML = to2Number(days)
     document.querySelector('.hours > .box').innerHTML = to2Number(hours)
